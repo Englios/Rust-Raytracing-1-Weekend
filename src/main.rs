03d07ced07de52b4a2c77fe8eea56109aa5e5ad2
Alif Aiman <alifaiman.edu@gmail.com>
2025-02-15 12:17:49 +0800

    Refactor image rendering to write output directly to a PPM file and improve progress reporting

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
-// use std::io;
+use std::{error::Error, fs::File, io::Write};
 
-fn main(){
-
-    //Image
-
+fn main() -> Result<(), Box<dyn Error>> {
     let image_width = 256;
     let image_height = 256;
 
-    //Render
-    println!("P3\n{} {}\n255", image_width, image_height);
+    // Create file
+    let mut file = File::create("./image.ppm")?;
+    
+    // Header
+    writeln!(file, "P3\n{} {}\n255", image_width, image_height)?;
 
-    //use loop to iterate over the rows of the image from top to bottom
+    // Render
     for j in 0..image_height {
-        for i in 0..image_width{
+        eprint!("\rScanlines remaining: {} ", image_height - j);  // Using eprint! for stderr
+        for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height -1) as f64;
-            let b = 0.0 as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0;
 
-            let ir = 255.99 * r; 
-            let ig = 255.99 * g;
-            let ib = 255.99 * b;
+            let ir = (255.999 * r) as i32; 
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
 
-            println!("{ir} {ig} {ib}")
-
+            writeln!(file, "{} {} {}", ir, ig, ib)?;
         }
     }
+
+    eprintln!("\rDone.                ");  // Using eprintln! for stderr
+
+    Ok(())
 }
