45dee3f5b299100df89e487f7df972195082db7b
Alif Aiman <alifaiman.edu@gmail.com>
2025-02-16 12:20:47 +0800

    refactor : Used function from color.rs to convert [0,1] to [0,255]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::{error::Error, fs::File, io::Write};
 
+mod vec3;
+mod color;
+use vec3::Vec3;
+use color::write_color;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let image_width = 256;
     let image_height = 256;
@@ -11,22 +16,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     writeln!(file, "P3\n{} {}\n255", image_width, image_height)?;
 
     // Render
-    for j in 0..image_height {
-        eprint!("\rScanlines remaining: {} ", image_height - j);  // Using eprint! for stderr
+    for j in (0..image_height) {
+        eprint!("\rScanlines remaining: {} ", image_height - j);
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
             let b = 0.0;
 
-            let ir = (255.999 * r) as i32; 
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
-
-            writeln!(file, "{} {} {}", ir, ig, ib)?;
+            let pixel_color = Vec3::new(r, g, b);
+            write_color(&mut file, pixel_color)?;
         }
     }
 
-    eprintln!("\rDone.                ");  // Using eprintln! for stderr
+    eprintln!("\rDone.                ");
 
     Ok(())
 }
