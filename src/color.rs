d3ae511a82cc1fab9895c80a5aa309e2d6389df3
Alif Aiman <alifaiman.edu@gmail.com>
2025-02-19 22:59:14 +0800

    Refactor write_color function to use Interval for clamping color values

--- src/color.rs
+++ src/color.rs
@@ -1,17 +1,20 @@
-use crate::vec3::Vec3;
+use crate::{interval::Interval, vec3::Vec3};
 use std::io::{self, Write};
 
 pub type Color = Vec3;
 
 pub fn write_color<W: Write>(writer: &mut W, pixel_color: Color) -> io::Result<()> {
+
     let r: f64 = pixel_color.x;
     let g: f64 = pixel_color.y;
     let b: f64 = pixel_color.z;
 
     // Convert from [0,1] to [0,255]
-    let r_byte = (255.999 * r) as i32;
-    let g_byte = (255.999 * g) as i32;
-    let b_byte = (255.999 * b) as i32;
+    let intensity = Interval::new(0.000, 0.999);
+    
+    let r_byte = (256 as f64* intensity.clamp(r)) as i32;
+    let g_byte = (256 as f64 * intensity.clamp(g)) as i32;
+    let b_byte = (256 as f64 * intensity.clamp(b)) as i32;
 
     writeln!(writer, "{} {} {}", r_byte, g_byte, b_byte)
 }
