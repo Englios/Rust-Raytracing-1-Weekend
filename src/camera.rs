7ecb2af6f4395f41fee944f2629ae51bec2a5042
Alif Aiman <alifaiman.edu@gmail.com>
2025-02-20 23:13:42 +0800

    Add max_depth parameter to Camera and update ray_color function for depth control

--- src/camera.rs
+++ src/camera.rs
@@ -9,6 +9,8 @@ pub struct Camera{
     pub aspect_ratio : f64,
     pub image_width : i32,
     pub samples_per_pixel : i32,
+    pub max_depth: i32,
+
     image_height :i32,
     pixel_samples_scale: f64,
     center : Point3,
@@ -24,6 +26,7 @@ impl Default for Camera {
             aspect_ratio: 1.0,
             image_width: 100,
             samples_per_pixel: 100,
+            max_depth:50,
             image_height: 100,
             pixel_samples_scale: 1.0,
             center: Point3::new(0.0, 0.0, 0.0),
@@ -35,11 +38,12 @@ impl Default for Camera {
 }
 
 impl Camera {
-    pub fn new(aspect_ratio:f64,image_width:i32,samples_per_pixel:i32) -> Self {
+    pub fn new(aspect_ratio:f64,image_width:i32,samples_per_pixel:i32,max_depth:i32) -> Self {
         Self {
             aspect_ratio,
             image_width,
             samples_per_pixel,
+            max_depth,
             image_height: 0, // Calculated based on aspect_ratio
             pixel_samples_scale:0.0,
             center: Point3::new(0.0, 0.0, 0.0),
@@ -72,14 +76,21 @@ impl Camera {
         self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_du + self.pixel_dv);
     }
 
-    fn ray_color(r: &Ray,world: &dyn Hittable) -> Color{
+    fn ray_color(r: &Ray,depth:i32 ,world: &dyn Hittable) -> Color{
+
+        if depth <= 0 {
+            return Color::new(0.0, 0.0, 0.0);
+        }
+
         let mut rec = HitRecord::default();
     
-        if world.hit(r, &Interval::new(0.0, INFINITY), &mut rec) {
-            let direction = Vec3::random_on_hemisphere(&rec.normal());
+        if world.hit(r, &Interval::new(0.001, INFINITY), &mut rec) {
+            // Lambertian Sphere's implemetation
+            let direction = rec.normal() + Vec3::random_unit_vector();
             
             return 0.5 * Camera::ray_color(
                 &Ray::new(rec.p(), direction),
+                depth,
                 world
             );
         }
@@ -117,8 +128,12 @@ impl Camera {
 
     pub fn render(&mut self, world: &dyn Hittable) -> io::Result<()> {
         self.initialize();
+
+        // Path to image output
+        let image_output_path = std::env::var("IMAGE_OUTPUT")
+                                                        .expect("IMAGE_OUTPUT must be set");
     
-        let file = File::create("./image.ppm")?;
+        let file = File::create(image_output_path)?;
         let mut writer = BufWriter::new(file);
         
         // Header
@@ -133,7 +148,11 @@ impl Camera {
 
                 for sample in 0..self.samples_per_pixel {
                     let r = self.get_ray(i, j);
-                    pixel_color += Camera::ray_color(&r, world);
+                    pixel_color += Camera::ray_color(
+                                                    &r,
+                                                    self.max_depth,
+                                                    world
+                                                );
                 }
 
                 write_color(
@@ -162,14 +181,14 @@ mod tests {
 
     #[test]
     fn test_camera_new() {
-        let camera = Camera::new(2.0, 200,100);
+        let camera = Camera::new(2.0, 200,100,10);
         assert_eq!(camera.aspect_ratio, 2.0);
         assert_eq!(camera.image_width, 200);
     }
 
     #[test]
     fn test_camera_initialize() {
-        let mut camera = Camera::new(2.0, 200,100);
+        let mut camera = Camera::new(2.0, 200,100,10);
         camera.initialize();
         assert_eq!(camera.image_height, 100); // 200/2.0 = 100
         assert!(camera.pixel_du.length() > 0.0);
