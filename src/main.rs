0bef375f0a95b64ec9dc0b287dfa3d8429e1bb34
Alif Aiman <alifaiman.edu@gmail.com>
2025-02-21 01:48:59 +0800

    Update sphere creation to use materials and adjust camera resolution

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ mod hittable;
 mod commons;
 mod camera;
 mod interval;
+mod material;
 
 use commons::INFINITY;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
+use material::Metal;
 use ray::Ray;
 use sphere::Sphere;
 use vec3::Vec3;
@@ -27,21 +29,30 @@ fn main() -> io::Result<()>{
 
     // World
     let mut world = HittableList::new();
+    use std::sync::Arc;
+    use material::Lambertian;
+    use color::Color;
+
+    let material_ground = Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Arc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
+    let material_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+
     let spheres: Vec<Box<dyn Hittable>> = vec![
         // Center sphere
-        Box::new(Sphere::new(Vec3::new(0.0, 0.0, -2.5), 0.5)),
+        Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.2), 0.5, material_center)),
         // Left sphere
-        Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5)),
+        Box::new(Sphere::new(Vec3::new(-1.0, 0.0, -1.0), 0.5, material_left)),
         // Right sphere
-        Box::new(Sphere::new(Vec3::new(1.0, 0.0, -3.5), 0.5)),
+        Box::new(Sphere::new(Vec3::new(1.0, 0.0, -1.0), 0.5, material_right)),
         // Ground sphere
-        Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0))
+        Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0, material_ground))
     ];
     world.add_objects(spheres);
 
     let mut cam = Camera::new(
                                         16.0/9.0, 
-                                        400,
+                                        900,
                                         100,
                                         50
                                     );
