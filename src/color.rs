b18653bc2f039f18e727d632ee4ae060b8e3ff4c
Alif Aiman <alifaiman.edu@gmail.com>
2025-02-20 23:13:49 +0800

    Update color writing to apply gamma correction and add linear_to_gamma function

--- src/color.rs
+++ src/color.rs
@@ -5,9 +5,9 @@ pub type Color = Vec3;
 
 pub fn write_color<W: Write>(writer: &mut W, pixel_color: Color) -> io::Result<()> {
 
-    let r: f64 = pixel_color.x;
-    let g: f64 = pixel_color.y;
-    let b: f64 = pixel_color.z;
+    let r: f64 = linear_to_gamma(pixel_color.x);
+    let g: f64 = linear_to_gamma(pixel_color.y);
+    let b: f64 = linear_to_gamma(pixel_color.z);
 
     // Convert from [0,1] to [0,255]
     let intensity = Interval::new(0.000, 0.999);
@@ -17,4 +17,13 @@ pub fn write_color<W: Write>(writer: &mut W, pixel_color: Color) -> io::Result<(
     let b_byte = (256 as f64 * intensity.clamp(b)) as i32;
 
     writeln!(writer, "{} {} {}", r_byte, g_byte, b_byte)
+}
+
+pub fn linear_to_gamma(linear_component:f64) -> f64{
+
+    if linear_component > 0.0 {
+        linear_component.sqrt()
+    } else {
+        0.0
+    }
 }
