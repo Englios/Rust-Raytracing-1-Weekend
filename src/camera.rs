c16637b1721f69bfad430698ca12f5c10a8eebb4
Alif Aiman <alifaiman.edu@gmail.com>
2025-02-22 01:49:12 +0800

    Enhance Camera struct with additional parameters and implement parallel rendering with progress bar

--- src/camera.rs
+++ src/camera.rs
@@ -1,23 +1,35 @@
 use super::*;
+use rayon::prelude::*;
 use crate::color::Color;
-use crate::commons::random_double;
+use crate::commons::{degrees_to_radians, random_double};
 use crate::interval::Interval;
 use crate::vec3::Point3;
 use std::io::{self,BufWriter,Write};
 use std::fs::File;
+use indicatif::ProgressBar;
+use std::sync::Arc;
 
+#[derive(Clone, Copy)]
 pub struct Camera{
     pub aspect_ratio : f64,
     pub image_width : i32,
     pub samples_per_pixel : i32,
     pub max_depth: i32,
 
+    pub vfov : f64,
+    pub lookfrom : Point3,
+    pub lookat: Point3,
+    pub vup: Vec3,
+
     image_height :i32,
     pixel_samples_scale: f64,
     center : Point3,
     pixel00_loc :Point3,
     pixel_du : Vec3,
-    pixel_dv : Vec3
+    pixel_dv : Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3
 }
 
 
@@ -28,29 +40,43 @@ impl Default for Camera {
             image_width: 100,
             samples_per_pixel: 100,
             max_depth:50,
+            vfov:90.0,
+            lookfrom: Point3::new(0.0, 0.0, 0.0),
+            lookat: Point3::new(0.0, 0.0, 0.0),
+            vup: Vec3::new(0.0, 1.0, 0.0),
             image_height: 100,
             pixel_samples_scale: 1.0,
             center: Point3::new(0.0, 0.0, 0.0),
             pixel00_loc: Point3::new(0.0, 0.0, 0.0),
             pixel_du: Vec3::new(0.0, 0.0, 0.0),
-            pixel_dv: Vec3::new(0.0, 0.0, 0.0)
+            pixel_dv: Vec3::new(0.0, 0.0, 0.0),
+            u: Vec3::default(),
+            v: Vec3::default(),
+            w: Vec3::default(),
         }
     }
 }
 
 impl Camera {
-    pub fn new(aspect_ratio:f64,image_width:i32,samples_per_pixel:i32,max_depth:i32) -> Self {
+    pub fn new(aspect_ratio:f64,image_width:i32,samples_per_pixel:i32,max_depth:i32,vfov:f64,lookfrom:Point3,lookat:Point3,vup:Vec3) -> Self {
         Self {
             aspect_ratio,
             image_width,
             samples_per_pixel,
             max_depth,
+            vfov,
+            lookfrom,
+            lookat,
+            vup,
             image_height: 0, // Calculated based on aspect_ratio
             pixel_samples_scale:0.0,
             center: Point3::new(0.0, 0.0, 0.0),
             pixel00_loc: Point3::new(0.0, 0.0, 0.0),
             pixel_du: Vec3::new(0.0, 0.0, 0.0),
-            pixel_dv: Vec3::new(0.0, 0.0, 0.0)
+            pixel_dv: Vec3::new(0.0, 0.0, 0.0),
+            u: Vec3::new(0.0, 0.0, 0.0),
+            v: Vec3::new(0.0, 0.0, 0.0),
+            w: Vec3::new(0.0, 0.0, 0.0),
         }
         
     }
@@ -60,20 +86,28 @@ impl Camera {
         self.image_height = if self.image_height < 1 {1} else {self.image_height};
         self.pixel_samples_scale = 1.0 / self.samples_per_pixel as f64;
     
-        self.center = Point3::new(0.0, 0.0, 0.0);
+        self.center = self.lookfrom;
     
         // View Port Dimensions
-        let focal_length = 0.5;  
-        let viewport_height = 2.0;
+        let focal_length = (self.lookfrom - self.lookat).length();  
+
+        let theta = degrees_to_radians(self.vfov);
+        let h = (theta/2.0).tan();
+        let viewport_height = 2.0 * h * focal_length;
         let viewport_width = viewport_height * ((self.image_width as f64) / (self.image_height as f64));
-    
-        let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-        let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+        // Basis Vectors for Camera
+        let w = (self.lookfrom - self.lookat).unit_vector();
+        let u = self.vup.cross(w).unit_vector();
+        let v = w.cross(u);
+        // Vectors for Viewport vertical and horizontal directions
+        let viewport_u = viewport_width * u;
+        let viewport_v = viewport_height * -v;
     
         self.pixel_du = viewport_u / self.image_width as f64;
         self.pixel_dv = viewport_v / self.image_height as f64;
     
-        let viewport_upper_left = self.center - Vec3::new(0.0, 0.0, focal_length) - viewport_u/2.0 - viewport_v/2.0;
+        let viewport_upper_left = self.center - (focal_length * w) - viewport_u/2.0 - viewport_v/2.0;
         self.pixel00_loc = viewport_upper_left + 0.5 * (self.pixel_du + self.pixel_dv);
     }
 
@@ -113,56 +147,80 @@ impl Camera {
         }
     }
 
-    fn get_ray(&self,i:i32,j:i32) -> Ray {
-        let offset = Camera::sample_square();
-
+    fn get_ray(&self, i: i32, j: i32) -> Ray {
         let pixel_center = self.pixel00_loc 
-                    + ((i as f64 + offset.x) * self.pixel_du) 
-                    + ((j as f64 + offset.y) * self.pixel_dv);
-
-        let ray_direction = (pixel_center - self.center).unit_vector();
-
-        Ray::new(self.center, ray_direction)
+            + (i as f64 * self.pixel_du)
+            + (j as f64 * self.pixel_dv);
+        let pixel_sample = pixel_center + self.pixel_sample_square();
+        let ray_origin = self.center;
+        let ray_direction = pixel_sample - ray_origin;
+        
+        Ray::new(ray_origin, ray_direction)
     }
 
+    fn pixel_sample_square(&self) -> Vec3 {
+        let px = -0.5 + random_double();
+        let py = -0.5 + random_double();
+        (px * self.pixel_du) + (py * self.pixel_dv)
+    }
 
     pub fn render(&mut self, world: &dyn Hittable) -> io::Result<()> {
         self.initialize();
 
-        // Path to image output
         let image_output_path = std::env::var("IMAGE_OUTPUT")
-                                                        .expect("IMAGE_OUTPUT must be set");
-    
+            .expect("IMAGE_OUTPUT must be set");
         let file = File::create(image_output_path)?;
         let mut writer = BufWriter::new(file);
         
-        // Header
         writeln!(writer, "P3\n{} {}\n255", self.image_width, self.image_height)?;
-    
-        // Render
-        for j in 0..self.image_height {
-            eprint!("\rScanlines remaining: {} ", self.image_height - j);
-            io::stderr().flush()?;
-            for i in 0..self.image_width {
-                let mut pixel_color = Color::new(0.0, 0.0, 0.0);
 
-                for sample in 0..self.samples_per_pixel {
-                    let r = self.get_ray(i, j);
-                    pixel_color += Camera::ray_color(
-                                                    &r,
-                                                    self.max_depth,
-                                                    world
-                                                );
-                }
+        // Cannot use self reference in Parallel Iteration,thus we must define them here first
+        let samples = self.samples_per_pixel;
+        let max_depth = self.max_depth;
+        let scale = self.pixel_samples_scale;
+        let image_width = self.image_width;
+        let pixel00_loc = self.pixel00_loc;
+        let pixel_du = self.pixel_du;
+        let pixel_dv = self.pixel_dv;
+        let center = self.center;
 
-                write_color(
-                    &mut writer, 
-                    pixel_color * self.pixel_samples_scale
-                )?;
-            }
+        // Add progress bar
+        let progress = std::sync::Arc::new(ProgressBar::new((self.image_height * self.image_width) as u64));
+        progress.set_style(
+            indicatif::ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} ({eta})")
+                .unwrap()
+        );
+
+        //Proggrs bar must be clone first to reference within parallel iterations
+        let progress_ref = Arc::clone(&progress);
+
+        //Store all pixels data in a List
+        let pixels: Vec<Color> = (0..self.image_height)
+            .into_par_iter()
+            .flat_map(move |j| {
+                let progress = progress.clone();
+                (0..image_width).into_par_iter().map(move |i| {
+                    let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+                    for _ in 0..samples {
+                        let pixel_center = pixel00_loc + (i as f64 * pixel_du) + (j as f64 * pixel_dv);
+                        let pixel_sample = pixel_center + ((random_double() - 0.5) * pixel_du + (random_double() - 0.5) * pixel_dv);
+                        let ray_direction = pixel_sample - center;
+                        let r = Ray::new(center, ray_direction);
+                        pixel_color += Camera::ray_color(&r, max_depth, world);
+                    }
+                    progress.inc(1);
+                    pixel_color * scale
+                })
+            })
+            .collect();
+
+        progress_ref.finish_with_message("Render complete");
+        // Write pixels to file
+        for pixel in pixels {
+            write_color(&mut writer, pixel)?;
         }
-    
-        eprintln!("\rDone.                ");
+
         Ok(())
     }
 }
@@ -181,17 +239,38 @@ mod tests {
 
     #[test]
     fn test_camera_new() {
-        let camera = Camera::new(2.0, 200,100,10);
+        let camera = Camera::new(
+            2.0, 
+            200,
+            100,
+            10,
+            90.0,
+            Point3::new(0.0, 0.0, -1.0),
+            Point3::new(0.0, 0.0, 0.0),
+            Vec3::new(0.0, 1.0, 0.0)
+        );
         assert_eq!(camera.aspect_ratio, 2.0);
         assert_eq!(camera.image_width, 200);
+        assert_eq!(camera.lookfrom, Point3::new(0.0, 0.0, -1.0));
+        assert_eq!(camera.lookat, Point3::new(0.0, 0.0, 0.0));
     }
 
     #[test]
     fn test_camera_initialize() {
-        let mut camera = Camera::new(2.0, 200,100,10);
+        let mut camera = Camera::new(
+            2.0, 
+            200,
+            100,
+            10,
+            90.0,
+            Point3::new(0.0, 0.0, -1.0),
+            Point3::new(0.0, 0.0, 0.0),
+            Vec3::new(0.0, 1.0, 0.0)
+        );
         camera.initialize();
         assert_eq!(camera.image_height, 100); // 200/2.0 = 100
         assert!(camera.pixel_du.length() > 0.0);
         assert!(camera.pixel_dv.length() > 0.0);
+        assert_eq!(camera.center, Point3::new(0.0, 0.0, -1.0));
     }
 }
