6009a5345af81b298cd45606d521610ec3469140
Alif Aiman <alifaiman.edu@gmail.com>
2025-02-24 23:42:22 +0800

    Refactor color handling: simplify write_color function and remove unused components

--- src/color.rs
+++ src/color.rs
@@ -1,33 +1,15 @@
-use crate::{interval::Interval, vec3::Vec3};
-use std::io::{self, Write};
+use crate::vec3::Vec3;
 
 pub type Color = Vec3;
 
-pub fn default() -> Vec3 {
-    Vec3::new(0.0, 0.0, 0.0)
-}
+pub fn write_color(pixel_color: &Color) -> (i32, i32, i32) {
+    let r = pixel_color.x() as f64;
+    let g = pixel_color.y() as f64;
+    let b = pixel_color.z() as f64;
 
-pub fn write_color<W: Write>(writer: &mut W, pixel_color: Color) -> io::Result<()> {
+    let r_byte = (255.999 * r) as i32;
+    let g_byte = (255.999 * g) as i32;
+    let b_byte = (255.999 * b) as i32;
 
-    let r: f64 = linear_to_gamma(pixel_color.x);
-    let g: f64 = linear_to_gamma(pixel_color.y);
-    let b: f64 = linear_to_gamma(pixel_color.z);
-
-    // Convert from [0,1] to [0,255]
-    let intensity = Interval::new(0.000, 0.999);
-    
-    let r_byte = (256 as f64* intensity.clamp(r)) as i32;
-    let g_byte = (256 as f64 * intensity.clamp(g)) as i32;
-    let b_byte = (256 as f64 * intensity.clamp(b)) as i32;
-
-    writeln!(writer, "{} {} {}", r_byte, g_byte, b_byte)
-}
-
-pub fn linear_to_gamma(linear_component:f64) -> f64{
-
-    if linear_component > 0.0 {
-        linear_component.sqrt()
-    } else {
-        0.0
-    }
+    (r_byte, g_byte, b_byte)
 }
