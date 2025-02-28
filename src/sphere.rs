4eca9780252b07bc485b3e93d1a68901cd4a6c74
Alif Aiman <alifaiman.edu@gmail.com>
2025-03-01 02:00:53 +0800

    test : Added sphere hit and miss tests to sphere.rs

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,178 +1,90 @@
-use std::sync::Arc;
-
-use crate::interval::Interval;
-use crate::material::Material;
-use crate::vec3::Point3;
-use crate::hittable::{Hittable,HitRecord};
+use crate::vec3::{Point3, Vec3};
 use crate::ray::Ray;
+use crate::hittable::{Hittable, HitRecord};
 
 
-#[derive(Clone)]
 pub struct Sphere {
-    center:Point3,
-    radius:f64,
-    mat: Arc<dyn Material>
+    center: Point3,
+    radius: f64,
+}
+
+impl Default for Sphere {
+    fn default() -> Self {
+        Self { center: Point3::default(), radius: 1.0 }
+    }
 }
 
 impl Sphere {
-    pub fn new(center:Point3,radius:f64,material: Arc<dyn Material>) -> Self{
-        Self {
-            center,
-            radius : f64::max(0.0,radius),
-            mat:material,
-        }
+    pub fn new(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
     }
 }
 
 impl Hittable for Sphere {
-    fn hit(&self,r:&Ray,t: &Interval,rec:&mut HitRecord) -> bool {
+    fn hit(
+        &self,
+        r: &Ray,
+        t_min: f64,
+        t_max: f64,
+        rec: &mut HitRecord,
+    ) -> bool {
         let oc = self.center - r.origin();
         let a = r.direction().length_squared();
-        let h: f64 = r.direction().dot(oc);
-        let c = oc.length_squared() - self.radius * self.radius;
+        let half_b = oc.dot(r.direction());
+        let c = oc.length_squared() - (self.radius * self.radius);
 
-        let discriminant = h*h - a*c;
-        if discriminant < 0.0 { 
-            return false; 
-        };
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return false;
+        }
 
         let sqrtd = discriminant.sqrt();
 
-        //Find nearest root in acceptable range
-        let mut root = (h - sqrtd) / a;
-        if !t.surrounds(root){
-            root = (h + sqrtd) / a;
-            
-            if!t.surrounds(root) {
+        // Find the nearest root that lies in the acceptable range.
+        let mut root = (half_b - sqrtd) / a;
+        if root <= t_min || root >= t_max {
+            root = (half_b + sqrtd) / a;
+            if root <= t_min || root >= t_max {
                 return false;
             }
         }
 
-        rec.set_t(root);
-        rec.set_p(r.at(rec.t()));
-        let outward_normal = ((rec.p() - self.center) / self.radius).unit_vector();
-        rec.set_face_normal(r, outward_normal);
-        rec.mat = self.mat.clone();
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = (rec.p - self.center) / self.radius;
 
-        true
+        return true;
     }
 
-    
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{commons::INFINITY,vec3::Vec3};
-    use crate::material::Metal;
 
-    #[test]
-    fn test_sphere_new() {
+    fn create_sphere() -> Sphere {
         let center = Point3::new(0.0, 0.0, 0.0);
         let radius = 1.0;
-        let material = Arc::new(Metal::new(Vec3::new(0.8, 0.8, 0.8),1.0));
-        let sphere = Sphere::new(center, radius, material.clone());
-
-        assert_eq!(sphere.center, center);
-        assert_eq!(sphere.radius, radius);
+        Sphere::new(center, radius)
     }
 
     #[test]
-    fn test_sphere_negative_radius() {
-        let center = Point3::new(0.0, 0.0, 0.0);
-        let radius = -1.0;
-        let material = Arc::new(Metal::new(Vec3::new(0.8, 0.8, 0.8),1.0));
-        let sphere = Sphere::new(center, radius, material.clone());
-
-        assert_eq!(sphere.radius, 0.0);
-    }
-
-    #[test]
-    fn test_ray_hits_sphere(){
-        let material = Arc::new(Metal::new(Vec3::new(0.8, 0.8, 0.8),1.0));
-        let sphere = Sphere::new(Point3::new(0.0, 0.0, 0.0), 1.0, material.clone());
-        let ray = Ray::new(
-            Point3::new(0.0, 0.0, -5.0),
-            Vec3::new(0.0, 0.0, 1.0)
-        );
+    fn test_sphere_hit() {  
+        let sphere = create_sphere();
+        let r = Ray::new(Point3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 1.0));
         let mut rec = HitRecord::default();
-
-        let hit = sphere.hit(&ray, &Interval::new(0.0, INFINITY), &mut rec);
-
+        let hit = sphere.hit(&r, 0.0, f64::INFINITY, &mut rec);
         assert!(hit);
-        assert_eq!(rec.t(), 4.0);
-        assert_eq!(rec.p(), Point3::new(0.0, 0.0, -1.0));
-        assert_eq!(rec.normal(), Vec3::new(0.0, 0.0, -1.0));
     }
 
     #[test]
-    fn test_ray_misses_sphere(){
-        let material = Arc::new(Metal::new(Vec3::new(0.8, 0.8, 0.8),1.0));
-        let sphere = Sphere::new(Point3::new(0.0, 0.0, 0.0), 1.0, material.clone());
-        let ray = Ray::new(
-            Point3::new(0.0, 0.0, -5.0),
-            Vec3::new(0.0, 1.0, 0.0)
-        );
+    fn test_sphere_hit_miss() {
+        let sphere = create_sphere();
+        let r = Ray::new(
+                Point3::new(1.0, 0.0, 0.0), 
+                Vec3::new(0.0, 0.0, 1.0));
         let mut rec = HitRecord::default();
-
-        let hit = sphere.hit(&ray,  &Interval::new(0.0, INFINITY), &mut rec);
-
+        let hit = sphere.hit(&r, 0.0, f64::INFINITY, &mut rec);
         assert!(!hit);
     }
-
-    #[test]
-    fn test_ray_hits_sphere_from_inside() {
-        let material = Arc::new(Metal::new(Vec3::new(0.8, 0.8, 0.8),1.0));
-        let sphere = Sphere::new(Point3::new(0.0, 0.0, 0.0), 1.0, material.clone());
-        let ray = Ray::new(
-            Point3::new(0.0, 0.0, 0.0),
-            Vec3::new(0.0, 0.0, -1.0)
-        );
-        let mut rec = HitRecord::default();
-
-        let hit = sphere.hit(&ray, &Interval::new(0.0, INFINITY), &mut rec);
-
-        assert!(hit);
-        assert_eq!(rec.t(), 1.0);
-        assert_eq!(rec.p(), Point3::new(0.0, 0.0, -1.0));
-        assert_eq!(rec.normal(), Vec3::new(0.0, 0.0, 1.0));
-    }
-
-    #[test]
-    fn test_ray_hits_sphere_at_t_min() {
-        let material = Arc::new(Metal::new(Vec3::new(0.8, 0.8, 0.8),1.0));
-        let sphere = Sphere::new(Point3::new(0.0, 0.0, 0.0), 1.0, material.clone());
-        let ray = Ray::new(
-            Point3::new(0.0, 0.0, -5.0),
-            Vec3::new(0.0, 0.0, 1.0)
-        );
-        let mut rec = HitRecord::default();
-
-        let hit = sphere.hit(&ray, &Interval::new(0.0, INFINITY), &mut rec);
-
-        assert!(hit);
-        assert_eq!(rec.t(), 4.0);
-        assert_eq!(rec.p(), Point3::new(0.0, 0.0, -1.0));
-        assert_eq!(rec.normal(), Vec3::new(0.0, 0.0, -1.0));
-    }
-
-    #[test]
-    fn test_ray_hits_sphere_at_t_max() {
-        let material = Arc::new(Metal::new(Vec3::new(0.8, 0.8, 0.8),1.0));
-        let sphere = Sphere::new(Point3::new(0.0, 0.0, -3.0), 1.0, material.clone());
-        let ray = Ray::new(
-            Point3::new(0.0, 0.0, -5.0),
-            Vec3::new(0.0, 0.0, 1.0)
-        );
-        let mut rec = HitRecord::default();
-
-        let hit = sphere.hit(&ray, &Interval::new(0.0, 3.0), &mut rec);
-
-        assert!(hit);
-        assert_eq!(rec.t(), 1.0);
-        assert_eq!(rec.p(), Point3::new(0.0, 0.0, -4.0));
-        assert_eq!(rec.normal(), Vec3::new(0.0, 0.0, -1.0));
-    }
-
 }
