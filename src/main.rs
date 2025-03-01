740a7364e7f1500c359bc8734d6cf1269724b057
Alif Aiman <alifaiman.edu@gmail.com>
2025-03-01 16:54:14 +0800

    feat: Configure camera with 100 samples per pixel for improved rendering quality

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ fn main() -> std::io::Result<()> {
     let mut camera = Camera::new();
     camera.aspect_ratio = 16.0 / 9.0;
     camera.image_width = 400;
+    camera.samples_per_pixel = 100;
     
     // Output file path
     let image_output_path = std::env::var("IMAGE_OUTPUT")
