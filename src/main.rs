6550df397b2a45b92877d1a235b333da8265c4e8
Alif Aiman <alifaiman.edu@gmail.com>
2025-03-01 16:30:24 +0800

    refactor: Move rendering logic to Camera module and simplify main function

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,17 @@ mod sphere;
 mod hittable_list;
 mod commons;
 mod interval;
+mod camera;
 
-use commons::INFINITY;
 use dotenv::dotenv;
 use hittable_list::HittableList;
 use hittable::{HitRecord, Hittable};
-use rayon::prelude::*;
 use commons::vec3::{Point3,Vec3};
 use sphere::Sphere;
 use std::fs::File;
 use std::io::BufWriter;
-use indicatif::ProgressBar;
+use camera::Camera;
 use std::sync::Arc;
-use std::io::Write;
-use commons::color::{Color,write_color};
 use commons::ray::Ray;
 
 
@@ -34,105 +31,23 @@ fn main() -> std::io::Result<()> {
     world.add_multiple(world_list);
 
     //Camera
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
-    let mut image_height = (image_width as f64 / aspect_ratio) as i32;
-    image_height = if image_height < 1 { 1 } else { image_height };
-
-    let focal_length = 1.0;
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * aspect_ratio;
-    let camera_center = Point3::new(0.0, 0.0, 0.0);
-    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-    let pixel_du = viewport_u / image_width as f64;
-    let pixel_dv = viewport_v / image_height as f64;
-    let viewport_upper_left = camera_center
-                                    - viewport_u / 2.0
-                                    - viewport_v / 2.0
-                                    - Vec3::new(0.0, 0.0, focal_length);
-    let pixel00_loc = viewport_upper_left
-                            + 0.5 * (pixel_du + pixel_dv);
-
-
-
+    let mut camera = Camera::new();
+    camera.aspect_ratio = 16.0 / 9.0;
+    camera.image_width = 400;
+    
     // Output file path
     let image_output_path = std::env::var("IMAGE_OUTPUT")
             .expect("IMAGE_OUTPUT must be set");
     let file = File::create(image_output_path)?;
     let mut writer = BufWriter::new(file);
 
-    // Write PPM header
-    writeln!(writer, "P3\n{} {}\n255", image_width, image_height)?;
+    camera.render(&mut writer, &world)?;
 
-
-    // Progress bar
-    let progress = std::sync::Arc::new(
-        ProgressBar::new((image_height * image_width) as u64)
-    );
-    // Set progress bar style
-    progress.set_style(
-        indicatif::ProgressStyle::default_bar()
-            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos}/{len} ({eta})")
-            .unwrap()
-    );
-    
-
-    // Clone progress bar for parallel processing
-    let progress_ref = Arc::clone(&progress);
-
-    //Parallel Processing of pixels
-    let pixels:Vec<(i32,i32,i32)> = (0..image_height)
-        .into_par_iter() // Parallel iterator
-        .flat_map(move |j| {
-            let progress = progress.clone(); // Clone progress bar for thread
-            let world = world.clone(); // Clone world for thread
-            (0..image_width).into_par_iter().map(move |i| {
-                // Calculate pixel color
-                let pixel_center = pixel00_loc
-                                    + i as f64 * pixel_du
-                                    + j as f64 * pixel_dv;
-                let ray_direction = pixel_center - camera_center;
-                let r = Ray::new(camera_center, ray_direction);
-                let pixel_color = ray_color(&r, &world);
-
-
-                // Increment progress bar
-                progress.inc(1);
-
-                // Write pixel color to file
-                let (r, g, b) = write_color(&pixel_color);
-                (r, g, b)
-            })
-        })
-        .collect();
-    
-    // Finish progress bar
-    progress_ref.finish_with_message("Render complete");
-
-    // Write pixels to file
-    for (r, g, b) in pixels {
-        writeln!(writer, "{} {} {}", r, g, b)?;
-    }
-
-    // Flush buffer to file
-    writer.flush()?;
     Ok(())
 }
 
 
-fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
-    let mut rec = HitRecord::default();
-    
-    if world.hit(r, 0.0, INFINITY, &mut rec) {
-        return 0.5 * (rec.normal() + Color::new(1.0, 1.0, 1.0));
-    }
 
-    let unit_direction = r.direction().unit_vector();
-    let a = 0.5 * (unit_direction.y() + 1.0);
-
-    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
-}
 
 fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = center - r.origin();
