a857ee1fea61e0da8be9599e91b25818446c30f0
Alif Aiman <alifaiman.edu@gmail.com>
2025-03-01 16:53:55 +0800

    feat: Add random number generation utilities to commons module

--- src/commons.rs
+++ src/commons.rs
@@ -2,9 +2,21 @@ pub mod color;
 pub mod vec3;
 pub mod ray;
 
+use rand;
+
 pub const INFINITY:f64 = f64::INFINITY; 
 pub const PI:f64 = 3.1415926535897932385;
 
-fn degree_to_rad(degrees:f64) -> f64 {
+pub fn degree_to_rad(degrees:f64) -> f64 {
     (degrees * PI) / 180.0
 }
+
+pub fn random_double() -> f64 {
+    // Returns a random real in [0,1).
+    rand::random::<f64>()
+}
+
+pub fn random_double_range(min:f64, max:f64) -> f64 {
+    min + (max - min) * random_double()
+}
+
