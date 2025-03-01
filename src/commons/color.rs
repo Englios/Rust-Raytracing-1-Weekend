51a4096eb1534c092e28591d0faf0b6a58563c7f
Alif Aiman <alifaiman.edu@gmail.com>
2025-03-01 16:54:04 +0800

    feat: Use Interval for color value clamping in write_color

--- src/commons/color.rs
+++ src/commons/color.rs
@@ -1,4 +1,5 @@
 use crate::commons::vec3::Vec3;
+use crate::interval::Interval;
 
 pub type Color = Vec3;
 
@@ -7,9 +8,11 @@ pub fn write_color(pixel_color: &Color) -> (i32, i32, i32) {
     let g = pixel_color.y() as f64;
     let b = pixel_color.z() as f64;
 
-    let r_byte = (255.999 * r) as i32;
-    let g_byte = (255.999 * g) as i32;
-    let b_byte = (255.999 * b) as i32;
+    let intensity = Interval::new(0.0, 0.999);
+
+    let r_byte = (255.999 * intensity.clamp(r)) as i32;
+    let g_byte = (255.999 * intensity.clamp(g)) as i32;
+    let b_byte = (255.999 * intensity.clamp(b)) as i32;
 
     (r_byte, g_byte, b_byte)
 }
