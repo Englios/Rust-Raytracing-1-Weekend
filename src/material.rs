c12b94bb3d7923d996bc6a1fc240cd73382ae266
Alif Aiman <alifaiman.edu@gmail.com>
2025-03-01 22:05:18 +0800

    feat: Add Dielectric material with basic refraction support

--- src/material.rs
+++ src/material.rs
@@ -73,5 +73,31 @@ impl Material for Metal {
     }
 }
 
+pub struct Dielectric {
+    refraction_index: f64,
+}
+
+impl Dielectric {
+    pub fn new(refraction_index: f64) -> Self {
+        Self { refraction_index }
+    }
+}
+
+impl Material for Dielectric {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool {
+        *attenuation = Color::new(1.0, 1.0, 1.0);
+        let refraction_ratio = if rec.front_face() { 
+            1.0 / self.refraction_index 
+        } else { 
+            self.refraction_index 
+        };
+
+        let unit_direction = r_in.direction().unit_vector();
+        let refracted = Vec3::refract(&unit_direction, &rec.normal, refraction_ratio);
+
+        *scattered = Ray::new(rec.p, refracted);
+        true
+    }
+}   
 
 
