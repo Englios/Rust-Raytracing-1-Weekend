6b8945878f4eb8821cdb00399f06129a879f43d4
Alif Aiman <alifaiman.edu@gmail.com>
2025-03-01 19:53:51 +0800

    feat: Add Metal material with reflection support

--- src/material.rs
+++ src/material.rs
@@ -44,3 +44,31 @@ impl Material for Lambertian {
     }
 }
 
+pub struct Metal {
+    albedo: Color,
+}
+
+impl Metal {
+    pub fn new(albedo: Color) -> Self {
+        Self { albedo }
+    }
+}
+
+impl Material for Metal {
+    fn scatter(
+        &self,
+        r_in: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool {
+        
+        let reflected = Vec3::reflect(r_in.direction().unit_vector(), rec.normal);
+        *scattered = Ray::new(rec.p, reflected);
+        *attenuation = self.albedo;
+        true
+    }
+}
+
+
+
